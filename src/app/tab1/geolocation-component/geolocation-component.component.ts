c24f08a9f38ae1ca074d2b129eaf0a51164dbb08
bl33d1 <bledi.bllaca12@gmail.com>
2024-08-19 15:43:51 +0200

    checkcheck

--- src/app/tab1/geolocation-component/geolocation-component.component.ts
+++ src/app/tab1/geolocation-component/geolocation-component.component.ts
@@ -3,7 +3,7 @@ import { Component, ElementRef, inject, OnInit, ViewChild } from '@angular/core'
 import { environment } from 'src/environments/environment';
 import { IonButton } from "@ionic/angular/standalone";
 import { async, BehaviorSubject, map, Observable, take, tap, timer } from 'rxjs';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, NgStyle } from '@angular/common';
 import { addDoc, collection, collectionData, CollectionReference, deleteDoc, doc, DocumentReference, Firestore } from '@angular/fire/firestore';
 
 @Component({
@@ -11,7 +11,7 @@ import { addDoc, collection, collectionData, CollectionReference, deleteDoc, doc
   templateUrl: './geolocation-component.component.html',
   styleUrls: ['./geolocation-component.component.scss'],
   standalone: true,
-  imports: [IonButton, HttpClientModule, AsyncPipe],
+  imports: [IonButton, HttpClientModule, AsyncPipe, NgStyle],
   providers: []
 })
 export class GeolocationComponentComponent implements OnInit {
@@ -40,7 +40,30 @@ export class GeolocationComponentComponent implements OnInit {
   ngOnInit(): void {
       
   }
-  
+
+  imageWidth: number = 400;
+  imageHeight: number = 400;
+
+  // Define the bounding box for Kosovo (approximate values)
+  kosovoBounds = {
+    minLat: 41.85,
+    maxLat: 43.26,
+    minLng: 20.03,
+    maxLng: 21.78
+  };
+
+  getPixelCoordinates(lat: string, lng: string) {
+
+    const xRatio = (Number(lng) - this.kosovoBounds.minLng) / (this.kosovoBounds.maxLng - this.kosovoBounds.minLng);
+    const yRatio = (Number(lat) - this.kosovoBounds.minLat) / (this.kosovoBounds.maxLat - this.kosovoBounds.minLat);
+
+    const xPixel = xRatio * this.imageWidth;
+    const yPixel = (1 - yRatio) * this.imageHeight; // Invert y-axis for top-left origin
+
+    return { x: xPixel, y: yPixel };
+  }
+
+
   getLocation() {
     if ('geolocation' in navigator) {
       navigator.geolocation.getCurrentPosition(
